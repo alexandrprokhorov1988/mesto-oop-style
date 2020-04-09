99c9d03897c55d40bbcbe1b26df95b55d5af5dbf
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-04-09 22:31:23 +0300

    feat: добавить профиль

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+const popupEditButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const nameInput = document.querySelector('[name=name]');
+const jobInput = document.querySelector('[name=profession]');
+const nameField = document.querySelector(".profile__user-name");
+const jobField = document.querySelector(".profile__user-profession");
+const formElement = document.querySelector(".form");
+const popupCloseIcon = document.querySelector(".popup__close-icon");
+
+function togglePopup() {
+    popup.classList.toggle("popup_opened");
+    nameInput.value = nameField.textContent;
+    jobInput.value = jobField.textContent;
+}
+
+function closePopup(event) {
+    if (event.target === popup || event.target === popupCloseIcon) {
+        popup.classList.toggle("popup_opened");
+    }
+}
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    nameField.textContent = nameInput.value;
+    jobField.textContent = jobInput.value;
+    togglePopup();
+}
+
+popupEditButton.addEventListener("click", togglePopup);
+popup.addEventListener("click", closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
