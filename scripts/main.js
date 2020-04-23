a686441d12413d743f54654cfae49aafb3c2e0f2
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-04-23 12:04:02 +0300

    feat: Добавить логику card

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,36 @@
+const initialCards = [
+  {
+    name: 'Дорога',
+    link: 'images/card-winter-6.jpg',
+    alt: 'Дорога.',
+  },
+  {
+    name: 'Дом',
+    link: 'images/card-winter-5.jpg',
+    alt: 'Дом.',
+  },
+  {
+    name: 'Деревья',
+    link: 'images/card-winter-4.jpg',
+    alt: 'Деревья.',
+  },
+  {
+    name: 'Пруд',
+    link: 'images/card-winter-3.jpg',
+    alt: 'Пруд под снегом.',
+  },
+  {
+    name: 'Солнце',
+    link: 'images/card-winter-2.jpg',
+    alt: 'Дорога в снегу.',
+  },
+  {
+    name: 'Открытая калитка',
+    link: 'images/card-winter-1.jpg',
+    alt: 'Открытая калитка.',
+  },
+];
+
 const popupEditButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseIcon = document.querySelector(".popup__close-icon");
@@ -6,7 +39,18 @@ const jobInput = document.querySelector('[name=profession]');
 const nameField = document.querySelector(".profile__user-name");
 const jobField = document.querySelector(".profile__user-profession");
 const formElement = document.querySelector(".form");
-const images = document.querySelectorAll('.card__img');
+const card = document.querySelector('#card').content;
+const sectionElements = document.querySelector('.elements');
+
+function cloneCards(name, link, alt = 'Картинка.') {
+  const cardElement = card.cloneNode(true);
+  const cardImg = cardElement.querySelector('.card__img');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImg.src = link;
+  cardImg.alt = alt;
+  cardTitle.textContent = name;
+  sectionElements.prepend(cardElement);
+}
 
 function togglePopup() {
   popup.classList.toggle("popup_opened");
@@ -27,21 +71,10 @@ function formSubmitHandler(event) {
   popup.classList.toggle("popup_opened");
 }
 
-function toggleImage() {
-  const element = document.querySelector(".elements");
-  let img = document.createElement('img');
-  img.classList.add('card__high-image');
-  img.setAttribute('src', this.getAttribute('src'));
-  img.setAttribute('alt', this.getAttribute('alt'));
-  img.addEventListener('click', function () {
-    img.remove();
-  });
-  element.insertAdjacentElement('beforeend', img);
-}
-
 popupEditButton.addEventListener("click", togglePopup);
 popup.addEventListener("click", closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-images.forEach(function (elem) {
-  elem.addEventListener('click', toggleImage);
+
+initialCards.forEach(element => {
+  cloneCards(element['name'], element['link'], element['alt'])
 });
